bf3446c0dcb2d69acec39434e5af713f7d0f94c3
atsumu22 <w.atsumu27@gmail.com>
2023-06-12 23:25:10 +0900

    set routing in header

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,32 +1,40 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
-import { Flex, Heading, Link, Box, IconButton } from "@chakra-ui/react"
-import { memo, FC } from 'react';
+import { memo, useCallback, FC } from 'react';
+import { Flex, Heading, Link, Box, useDisclosure } from "@chakra-ui/react"
+import { useHistory } from 'react-router-dom';
+
+import { MenuIconButton } from "../../atoms/MenuIconButton";
+import { MenuDrawer } from '../../molecules/MenuDrawer';
 
 export const Header: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push("/home"), [history]);
+  const onClickUserManagement = useCallback(() => history.push("/home/user_management"), [history]);
+  const onClickSetting = useCallback(() => history.push("/home/setting"), [history]);
+
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザー管理アプリ</Heading>
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザー管理アプリ</Heading>
+        </Flex>
+        <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+          <Box pr={4}>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
+          </Box>
+          <Link onClick={onClickSetting}>設定</Link>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
-      </Flex>
-      <IconButton
-        aria-label="メニューボタン"
-        icon={<HamburgerIcon />}
-        size="md" variant="unstyled"
-        display={{ base: "block", md: "none" }}
-      />
-    </Flex>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} onClickHome={onClickHome} onClickUserManagement={onClickUserManagement} onClickSetting={onClickSetting} />
+    </>
   )
 });
