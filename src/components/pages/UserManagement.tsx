51a50f0184356a6ad79d8a26d81adb70f30fc4e2
atsumu22 <w.atsumu27@gmail.com>
2023-06-14 16:12:30 +0900

    styled content

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,30 @@
-import { memo, FC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { memo, FC, useEffect } from "react";
+import { Box, Image, Text, Stack, Wrap, WrapItem, Spinner, Center } from '@chakra-ui/react'
+import { UserCard } from "../organisms/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>UserManagementページです</p>
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+          <Center h="100vh">
+            <Spinner />
+          </Center>
+        ) : (
+          <Wrap p={{ base: 4, md: 10 }}>
+            {users.map((user) => (
+              <WrapItem key={user.id} mx="auto">
+                <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+              </WrapItem>
+            ))}
+          </Wrap>
+      )}
+    </>
+  );
 });
